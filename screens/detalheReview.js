33b8ab9d6cd362a8588db7e2ed8a9a228a6fc438
NicholasGCamargo <nicholascamargo@gmail.com>
2020-06-07 15:43:23 -0300

    adicionado estilo global

--- screens/detalheReview.js
+++ screens/detalheReview.js
@@ -1,16 +1,13 @@
 import React from "react";
 import { StyleSheet, View, Text } from "react-native";
+import { globalStyles } from "../styles/global";
 
 export default function DetalheReview() {
   return (
-    <View style={styles.container}>
-      <Text>detalhe Screen</Text>
+    <View style={globalStyles.container}>
+      <Text style={globalStyles.titleText}>detalhe Screen</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    padding: 24,
-  },
-});
+const styles = StyleSheet.create({});
--- screens/sobre.js
+++ screens/sobre.js
@@ -1,16 +1,13 @@
 import React from "react";
 import { StyleSheet, View, Text } from "react-native";
+import { globalStyles } from "../styles/global";
 
 export default function Sobre() {
   return (
     <View style={styles.container}>
-      <Text>sobre Screen</Text>
+      <Text style={globalStyles.titleText}>sobre Screen</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    padding: 24,
-  },
-});
+const styles = StyleSheet.create({});
